bb08a04c04de63c71ee49f83cd5fb46a1264a9ed
alejoi78 <torojohnalejandro7@gmail.com>
2025-11-27 13:36:44 -0500

    Configuracion base de datos con Code First

---
+++ API-JohnToro/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API_JohnToro.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_JohnToro.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+    
---
+++ API-JohnToro/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_JohnToro.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required]
+        [Display(Name="Nombre de la categoria")]
+        public string Name { get; set; }
+    }
+}
